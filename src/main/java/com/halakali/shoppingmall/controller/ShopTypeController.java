48e490b2274694f114ca36e0f6c98fa9fb973b64
hala-k-ali <hala.1.ali@outlook.com>
2026-08-02 16:33:20 -0400

    Add agent config and ShopTypeController skeleton

---
+++ src/main/java/com/halakali/shoppingmall/controller/ShopTypeController.java
@@ -0,0 +1,17 @@
+package com.halakali.shoppingmall.controller;
+
+/**
+ * Plain Java skeleton controller for ShopType.
+ *
+ * Created by agent: add-shop-type-controller-agent
+ */
+public class ShopTypeController {
+
+    /**
+     * Default constructor.
+     */
+    public ShopTypeController() {
+    }
+
+    // TODO: add methods (list/get/create/update/delete) if/when needed.
+}
